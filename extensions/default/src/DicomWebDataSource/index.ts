158e7472541ae1d9ce8c27b6f1d0fdfbfcd0efa0
Eugene Sodin <c.eugene.sodin@self.inc>
2025-04-29 15:08:40 +0300

    - Add abilities to delete and download studies

--- extensions/default/src/DicomWebDataSource/index.ts
+++ extensions/default/src/DicomWebDataSource/index.ts
@@ -172,6 +172,7 @@ function createDicomWebApi(dicomWebConfig: DicomWebConfig, servicesManager) {
       wadoDicomWebClient = dicomWebConfig.staticWado
         ? new StaticWadoClient(wadoConfig)
         : new api.DICOMwebClient(wadoConfig);
+      implementation.retrieve.customClient = wadoDicomWebClient as StaticWadoClient;
     },
     query: {
       studies: {
@@ -214,6 +215,7 @@ function createDicomWebApi(dicomWebConfig: DicomWebConfig, servicesManager) {
       },
     },
     retrieve: {
+      customClient: null as unknown as StaticWadoClient,
       /**
        * Generates a URL that can be used for direct retrieve of the bulkdata
        *
